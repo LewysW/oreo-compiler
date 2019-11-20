e60b3c67e4b5c5eb8a919b30072ac5c8298b5e34
lewys <lewysward99@gmail.com>
2019-11-20 00:25:47 +0000

    Added TODO

--- src/main.cpp
+++ src/main.cpp
@@ -18,32 +18,32 @@ int main(int argc, char* argv[]) {
         //Perform lexical analysis on file
         //and print tokens
         Lexer lexer(fileName);
-        //lexer.printTokens();
+        lexer.printTokens();
 
         //Perform syntactic analysis on file
         //and print abstract syntax tree
         Parser parser(lexer.getTokenList());
-        //parser.printTree();
+        parser.printTree();
 
         //Perform semantic analysis on parse tree
         Semantic semantic;
         semantic.analyse(parser.getParseTree());
         //Print scopes and symbol tables
-        //semantic.printTree(semantic.getGlobalScope());
+        semantic.printTree(semantic.getGlobalScope());
 
         //Perform type checking on parse tree
         TypeChecker typeChecker;
         typeChecker.checkTypes(parser.getParseTree(), semantic.getGlobalScope());
 
         //TODO list:
+        //TODO - Else not allocated correctly in parser - FIX!
+
         //TODO - report on type checking
 
         //TODO - Pass queue through type checking code and add instructions for ease of TAC generation
 
         //TODO - three address code generation
         //TODO - report on three address code generation
-        //TODO - HLA generation
-        //TODO - report on HLA generation
     }
 
     return 0;
