6aee862bb51f4d456bbcaf446d8cc063df6c02a0
lewys <lewysward99@gmail.com>
2019-11-21 03:19:09 +0000

    Added comments to Instruction files

--- src/tac_generation/Instruction.cpp
+++ src/tac_generation/Instruction.cpp
@@ -1,36 +1,75 @@
 #include "Instruction.h"
 
+/**
+ * Getter for operation
+ * @return operation
+ */
 const std::string& Instruction::getOp() const {
     return op;
 }
 
+/**
+ * Getter for arg1
+ * @return arg1
+ */
 const std::string &Instruction::getArg1() const {
     return arg1;
 }
 
+/**
+ * Getter for arg2
+ * @return arg2
+ */
 const std::string &Instruction::getArg2() const {
     return arg2;
 }
 
+/**
+ * Getter for result
+ * @return result
+ */
 const std::string &Instruction::getResult() const {
     return result;
 }
 
+/**
+ * Constructor for Instruction
+ * @param op - operation of instruction
+ * @param arg1 - first argument of instruction
+ * @param arg2 - second argument of instruction
+ * @param result - result of instruction
+ */
 Instruction::Instruction(const std::string& op, const std::string &arg1, const std::string &arg2,
                          const std::string &result) : op(op), arg1(arg1), arg2(arg2), result(result), label(std::string("")) {}
 
+/**
+ * Getter for label
+ * @return label
+ */
 const std::string &Instruction::getLabel() const {
     return label;
 }
 
+/**
+ * Setter for label
+ * @param label
+ */
 void Instruction::setLabel(const std::string &label) {
     Instruction::label = label;
 }
 
+/**
+ * Getter for branchInstruction
+ * @return whether instruction is branch instruction or not
+ */
 bool Instruction::isBranchInstruction() const {
     return branchInstruction;
 }
 
+/**
+ * Setter for branchInstruction
+ * @param branchInstruction
+ */
 void Instruction::setBranchInstruction(bool branchInstruction) {
     Instruction::branchInstruction = branchInstruction;
 }
--- src/tac_generation/Instruction.h
+++ src/tac_generation/Instruction.h
@@ -2,32 +2,53 @@
 
 #include "../parser/Pattern.h"
 
+/**
+ * Models a single three address code instruction (as a quadruple)
+ */
 class Instruction {
 private:
+    //quadruple:
+
+    //Stores operation of instruction
     std::string op;
+    //Stores first argument of instruction
     std::string arg1;
+    //Stores second argument of instruction
     std::string arg2;
+    //Stores result of instruction
     std::string result;
 
+    //Optional label for conditional branching
     std::string label;
+
+    //Whether instruction involves branching
     bool branchInstruction = false;
 public:
+    //Constructor for instruction
     Instruction(const std::string& op, const std::string &arg1, const std::string &arg2, const std::string &result);
 
+    //Getter for operation
     const std::string& getOp() const;
 
+    //Getter for arg1
     const std::string &getArg1() const;
 
+    //Getter for arg2
     const std::string &getArg2() const;
 
+    //Getter for result
     const std::string &getResult() const;
 
+    //Getter for label
     const std::string &getLabel() const;
 
+    //Setter for label
     void setLabel(const std::string &label);
 
+    //Returns whether instruction is branch instruction
     bool isBranchInstruction() const;
 
+    //Setter for branchInstruction
     void setBranchInstruction(bool branchInstruction);
 
 
